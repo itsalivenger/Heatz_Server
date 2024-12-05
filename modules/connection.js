919e1f432fe7a4eabb53c282d9be2de8fdd118de
itsalivenger <alihoussa16@gmail.com>
2024-12-05 01:36:17 +0100

    updated logo finally

--- modules/connection.js
+++ modules/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectToDb = async () => {
   try {
-    const client = new MongoClient(process.env.MONGODB_URI);
+    const client = new MongoClient("mongodb+srv://emailtrash226:MIyX5Tb2o1F6jCO9@clientsali.dfld8.mongodb.net/?retryWrites=true&w=majority&appName=ClientsAli");
     await client.connect(); // Ensure the client connects successfully
     console.log('MongoDB connected successfully');
     return client;
