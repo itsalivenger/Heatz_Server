764659d1449325d6ab61380ad046e1aa04b03150
itsalivenger <alihoussa16@gmail.com>
2024-12-30 11:10:26 +0100

    updated server

--- modules/multerConfig.js
+++ modules/multerConfig.js
@@ -1,33 +1,22 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
-// Configure Multer storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const uploadPath = 'products/';
+        const uploadPath = path.join(__dirname, 'products');
 
         // Check if the folder exists; if not, create it
         if (!fs.existsSync(uploadPath)) {
+            console.log(`Creating directory: ${uploadPath}`);
             fs.mkdirSync(uploadPath, { recursive: true }); // Ensures that nested folders are created if needed
         }
 
         cb(null, uploadPath); // Save files to 'products' folder
     },
     filename: function (req, file, cb) {
-        const uniqueName = Date.now() + path.extname(file.originalname); // Generate a unique filename
-        const filePath = path.join('products', uniqueName);
-
-        // Check if the file already exists
-        if (fs.existsSync(filePath)) {
-            return cb(new Error('File with the same name already exists'));
-        }
-
+        const uniqueName = Date.now() + path.extname(file.originalname);
+        console.log(`Saving file to: ${path.join('products', uniqueName)}`);
         cb(null, uniqueName); // Save the file with the unique name
     }
 });
 
-// File validation
 const upload = multer({
     storage,
     fileFilter: (req, file, cb) => {
