435a5aaa2072544dd5e77ec025127425b6a0b7ed
itsalivenger <alihoussa16@gmail.com>
2025-01-03 04:03:24 +0100

    m

--- modules/cloudinaryHelper.js
+++ modules/cloudinaryHelper.js
@@ -2,7 +2,6 @@ const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 
 // Configure Cloudinary with your credentials
-console.log(process.env.CLOUDINARY_CLOUD_NAME);
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
